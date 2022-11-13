790e5ed8bcaa8f22f159a61ee21b2520dcb218bc
Gartham <gartham@dusttoash.org>
2022-11-12 21:06:07 -0600

    Update Canvas.java

--- src/com/gartham/fluidsim/Canvas.java
+++ src/com/gartham/fluidsim/Canvas.java
@@ -16,7 +16,9 @@ public class Canvas {
 			int posy = (int) (((double) yfrac / Integer.MAX_VALUE + 1) * field.getHeight() / 2);
 
 			var vec = field.get(posx, posy);
-			System.out.println(vec);
+
+			System.out.println(vec + " APPLIED TO " + this);
+			push(vec);
 		}
 
 		public void push(Vector vec) {
@@ -31,9 +33,33 @@ public class Canvas {
 					+ ')';
 		}
 
+		/**
+		 * Moves this particle by the ratios specified. A ratio of <code>1</code> moves
+		 * the particle one full length (for the x-axis) and one full height (for the
+		 * y-axis). The ratio properties specified in a call to this method represent
+		 * the fractions of the canvas that the particle will move. For moving the
+		 * particle in absolute units (<code>1/{@link Integer#MAX_VALUE}</code> of the
+		 * canvas&#8211;sized steps), use {@link #moveByAbsolute(int, int)}.
+		 * 
+		 * 
+		 * @param xRatio The proportion of the length of the canvas to move (rightwards,
+		 *               along the x axis).
+		 * @param yRatio The proportion of the height of the canvas to move (downwards,
+		 *               along the y axis).
+		 */
+		public void moveByRelative(double xRatio, double yRatio) {
+			xfrac += xRatio * Integer.MAX_VALUE;
+			yfrac += yRatio * Integer.MAX_VALUE;
+		}
+
+		public void moveByAbsolute(int unitXSteps, int unitYSteps) {
+			xfrac += unitXSteps;
+			yfrac += unitYSteps;
+		}
+
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		Canvas can = new Canvas();
 		Particle particulate = can.new Particle(0xFF0000ff, 0, 0);
 
@@ -42,7 +68,10 @@ public class Canvas {
 			for (int j = 0; j < field.getHeight(); j++)
 				field.set(i, j, new Vector(i, j));
 
-		particulate.flow(field);
+		while (true) {
+			Thread.sleep(1000);
+			particulate.flow(field);
+		}
 	}
 
 }
