d23c510ab84d88eb175dab5d7860aecc99d02b57
Gartham <gartham@dusttoash.org>
2022-11-30 00:27:43 -0600

    Update Vector.java

--- src/com/gartham/fluidsim/Vector.java
+++ src/com/gartham/fluidsim/Vector.java
@@ -36,4 +36,35 @@ public class Vector {
 		return "Vector <" + x + ", " + y + '>';
 	}
 
+	public Vector add(Vector other) {
+		return new Vector(x + other.x, y + other.y);
+	}
+
+	/**
+	 * Performs <code>this - other</code>. Subtracts the specified,
+	 * <code>other</code> vector, from this one and returns the result.
+	 * 
+	 * @param other
+	 * @return
+	 */
+	public Vector subtract(Vector other) {
+		return new Vector(x - other.x, y - other.y);
+	}
+
+	public Vector times(double scale) {
+		return new Vector(x * scale, y * scale);
+	}
+
+	public Vector negX() {
+		return new Vector(-x, y);
+	}
+
+	public Vector negY() {
+		return new Vector(x, -y);
+	}
+
+	public Vector negXY() {
+		return new Vector(-x, -y);
+	}
+
 }
